68f29baf07bdf9b4bbfff48fab9b147d732cdd06
TSDilov <cv.dilov@abv.bg>
2022-04-23 11:50:06 +0300

    Added interface for the board, printing on indexes when player mark with symbol and symbols for the game

---
+++ IBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public interface IBoard
+    {
+        bool IsFull();
+
+        void PlaceSymbol(IndexOnBoard index, SymbolOnBoard symbol);
+
+        IEnumerable<IndexOnBoard> GetEmptyPositions();
+
+        SymbolOnBoard GetRowSymbol(int row);
+        SymbolOnBoard GetColumnSymbol(int column);
+        SymbolOnBoard GetDiagonalFromTopLeftSymbol();
+        SymbolOnBoard GetDiagonalFromBottomLeftSymbol();
+    }
+}
---
+++ IndexOnBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class IndexOnBoard
+    {
+        public IndexOnBoard(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+        public IndexOnBoard(string str)
+        {
+            var parts = str.Split(',');
+            this.Row = int.Parse(parts[0]);
+            this.Column = int.Parse(parts[1]);
+        }
+        public override bool Equals(object obj)
+        {
+            var otherIndex = obj as IndexOnBoard;
+
+            return this.Row == otherIndex.Row &&
+                this.Column == otherIndex.Column;
+        }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public override string ToString()
+        {
+            return $"{this.Row},{this.Column}";
+        }
+    }
+}
